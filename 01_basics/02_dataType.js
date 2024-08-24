7d0400cf95e357bcb4f9196fa45fb24eb972d2b4
Chandan Paul <113237056+chandanpaul123@users.noreply.github.com>
2024-08-24 14:16:35 +0000

    dataType

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,27 @@
+"use strict"; //treate all JS code in newer version
+
+// alert(3+3) //we are using nodejs, not browser
+
+console.log("Chandan")
+console.log(3+3)
+
+
+let name = "Chandan"
+let age = 18
+let isLoggedIn = false
+let state;
+
+
+//number => 2 to power of 53
+//bigint
+//string => ""
+//boolean => true/false
+//nill => standalone value
+//undefined => 
+//symbol=> unique
+
+//object
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
