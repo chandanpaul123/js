405d48c3e47c8c3980d437fa345bdadcb315120d
Chandan Paul <113237056+chandanpaul123@users.noreply.github.com>
2024-09-12 16:50:47 +0000

    new iife

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -11,4 +11,9 @@
 (  (name) => {
     //unnamed IIFE
     console.log(`From Me ${name}`);
-}  )('Chandan')
+}  )('Chandan');
+
+(add = (num1,num2) => {
+      return num1 + num2  
+})
+console.log(add(6, 7));
