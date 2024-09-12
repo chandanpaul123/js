b2515cfcd0151857e36e6e448caf69b563770bf0
Chandan Paul <113237056+chandanpaul123@users.noreply.github.com>
2024-09-12 16:23:54 +0000

    Arrow

--- 03_basics/03_Arrow.js
+++ 03_basics/03_Arrow.js
@@ -12,4 +12,39 @@ const user = {
 // user.welcomeMessage()
 // user.username = "Suman"
 // user.welcomeMessage()
-console.log(this);
+// console.log(this);
+
+/*function chandan() {
+    // let num12 = "chandu"
+    console.log(this.num12);
+}
+chandan()*/
+
+
+// const chandan = function () {
+//     let num12 = "chandu"
+//     console.log(this.num12);
+// }
+
+// const chandan = ()=> {
+//     let user = "chandu"
+//     console.log(this);
+// }
+// chandan()
+
+// syntax -> const chandan = ()=> {}
+
+let num1 = 2
+let num2 = 3
+
+let addTwo = ()=> {return num1 + num2;} // Explicit return
+//if you console.log(addTwo()); o/p -> NaN
+// console.log(addTwo(num1, num2)); //5
+
+//implicit return
+// const add = (num1,num2) => (num1+num2) 
+// console.log(add(num1,num2));
+
+const add = (num1,num2) => ({username:"Chandan"})
+console.log(add(num1,num2));
+
